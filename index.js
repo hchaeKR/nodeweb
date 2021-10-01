9534a808f3d5722ac8856feefaec19eead8d8ed4
hchae <hongil.chae@aol.com>
2021-10-01 18:17:21 +0900

    ejs 적용

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //name: index.js
 
-const { request } = require('express');
 var express = require('express');
 var app = express();
 
@@ -8,7 +7,24 @@ var app = express();
 //     res.send('hello express');
 // });
 
+// app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function (req, res) {
+    res.render('hello', {name:req.query.nameQuery});
+});
+
+app.get('/hello/:nameParam', function (req, res) {
+    res.render('hello', {name:req.params.nameParam});
+});
+
+/**
+ * req.query - GET 방식 query string
+ * req.params - GET 방식 URI
+ * req.body - POST 방식 key-value property ex) req.body.key
+ */
 
 var port = 3000;
 app.listen(port, function () {
